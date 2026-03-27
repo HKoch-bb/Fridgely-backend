b47ebc45cb67d8685615899fcc1574ed873d1db4
hemanth k <kocherlakotahemanth@gmail.com>
2026-03-26 19:37:00 -0700

    fix: remove next param from async mongoose pre-save hook

--- server.js
+++ server.js
@@ -33,9 +33,10 @@ const userSchema = new mongoose.Schema({
   pageActivity: [{ page: String, timestamp: { type: Date, default: Date.now } }],
 }, { timestamps: true });
 
-userSchema.pre("save", async function(next) {
-  if (this.isModified("password")) this.password = await bcrypt.hash(this.password, 12);
-  next();
+userSchema.pre("save", async function() {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
